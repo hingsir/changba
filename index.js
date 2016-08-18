249eaa977f324ac77972914e89e90947d63d54d3
hingsir <hingsir1024@gmail.com>
2016-08-18 13:50:05 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var ls = require('./lib/localstorage.js')
 var jsdom = require("jsdom")
 var fs = require("fs")
 var ejs = require('ejs')
+var crypto = require('crypto')
 var jquery = fs.readFileSync("./node_modules/jquery/dist/jquery.js", "utf-8")
 
 var uid = process.argv[2] || '242071630'
@@ -81,8 +82,22 @@ function renderFile(songs) {
     ejs.renderFile('views/index.html', {
         title: '聆听笛箫 - 寒江雪🎵',
         songs: songs,
-        version: Math.random().toString(16).slice(-6)
-    }, function(err, result) {
+        version: fileMD5('./static/style.css')
+    }, (err, result) => {
         fs.writeFile('s/index.html', result, 'utf-8')
     })
+}
+function fileMD5(path, callback) {
+    let hash = crypto.createHash('md5');
+    if (callback && typeof callback == 'function') {
+        fs.readFile(path, (err, result) => {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, hash.update(result).digest('hex'));
+            }
+        })
+    } else {
+        return hash.update(fs.readFileSync(path)).digest('hex');
+    }
 }
